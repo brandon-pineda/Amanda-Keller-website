8cbbff0235127678e1f55e522eedcac331effbcf
Brandon Pineda <bppsparton@gmail.com>
2016-11-19 15:35:16 -0800

    added a hamburger menu for mobile

--- resource/js/script.js
+++ resource/js/script.js
@@ -1,2 +1,16 @@
 $(document).ready(function() {
-}
+    $('#mobile-nav-icon').click(function(){
+        var navList = $('#navbar li');
+        var icon = $('#mobile-nav-icon i');
+
+        navList.toggle();
+
+        if (icon.hasClass('fa fa-bars')) {
+            icon.removeClass('fa fa-bars');
+            icon.addClass('fa fa-times');
+        } else {
+            icon.removeClass('fa fa-times');
+            icon.addClass('fa fa-bars');
+            }
+    });
+});
