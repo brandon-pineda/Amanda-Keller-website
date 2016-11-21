38eab9f097352ec2d14a9855f8475641b373f704
Brandon Pineda <bppsparton@gmail.com>
2016-11-20 16:48:01 -0800

    adding images, and a sticky nav. sticky nav has positioning issues and navbar is invisible when going from mobile to desktop sizing

--- resource/js/script.js
+++ resource/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('#mobile-nav-icon').click(function(){
-        var navList = $('#navbar li');
+        var navList = $('#intro-container nav li');
         var icon = $('#mobile-nav-icon i');
 
         navList.toggle();
@@ -12,5 +12,17 @@ $(document).ready(function() {
             icon.removeClass('fa fa-times');
             icon.addClass('fa fa-bars');
             }
+    });
+
+    for(i=1;i<=20;i++) {
+        $('#portfolio-container').append('<img src="resource/img/amanda/' + i + '.jpg">');
+    };
+
+    $(window).bind('scroll', function () {
+    if ($(window).scrollTop() > 340) {
+        $('#intro-container nav').attr('id' , 'fixed');
+    } else {
+        $('#intro-container nav').removeAttr('id');
+    }
     });
 });
